4d63da16b9dda10bac4bcda6d442f17645b25d48
Anjali Kedia <24kedia.anjali@gmail.com>
2023-04-28 22:37:39 +0530

    Methods:POST,PUT,Delete

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,22 +1,44 @@
 const express = require('express');
 const app = express()
-const logger = require('./logger.js')
-const authorize = require('./authorize.js')
-//req => middleware => res
+let {people} = require('./data')
 
-app.use([logger,authorize])
+// static assests
+app.use(express.static('./methods-public'))
 
-app.get('/',(req,res)=>{
-    res.send('Home')
+
+//parse form data
+//(to get the data from the site, the data that you enter)
+app.use(express.urlencoded({extended: false}))
+
+//parse json
+app.use(express.json())
+
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({success:true,data:people})
 })
-app.get('/about',(req,res)=>{
-    res.send('About')
+app.post('/api/people',(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false,msg:'please provide name value'})
+    }
+
+    res.status(201).json({success:true,person:name})
 })
-app.get('/api/products',(req,res)=>{
-    res.send('Products')
+app.post('api/postman/people',(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false,msg:'please provide name value'})
+    }
+    res.status(201).json({success:true,data:[...people,name]})
+    
 })
-app.get('/api/items',(req,res)=>{
-    res.send(req.user)
+
+app.post('/login',(req,res)=>{
+    const {name} = req.body;
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please provide Credentials')
 })
 app.listen(2000,()=>{
     console.log('App is listening on port 2000..')
